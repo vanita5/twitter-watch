8a5175a3305c70f52d5efe92140f3d265fe2a78b
vanita5 <mail@vanita5.de>
2016-03-27 05:12:19 +0200

    implement status insertion

--- background/index.js
+++ background/index.js
@@ -1,5 +1,4 @@
 var MongoClient = require('mongodb').MongoClient,
-    assert = require('assert'),
     accounts = require('require-all')(__dirname + '/../config/accounts'),
     twitter_config = require(__dirname + '/../config/twitter'),
     Twit = require('twit');
@@ -20,15 +19,25 @@ var T = new Twit({
     access_token_secret: twitter_config.auth.access_token_secret
 });
 
-var stream = T.stream('statuses/filter', { follow: s_accounts });
-
-stream.on('tweet', function(tweet) {
-    console.log(tweet.user.screen_name);
-    console.log(tweet.text);
-});
-
 MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
+    if (err) {
+        return console.dir(err);
+    }
     console.log('Connected correctly to server.');
-    db.close();
+
+    var tweets = db.collection('tweets');
+
+    var stream = T.stream('statuses/filter', { follow: s_accounts });
+
+    stream.on('tweet', function(tweet) {
+        console.log('Inserting @' + tweet.user.screen_name + ': "' + tweet.text + '"');
+        tweets.insertOne(tweet, {w:1}, function(err, result) {
+            if (err) console.error(err);
+        });
+    });
+
+    stream.on('delete', function(deleteMessage) {
+       //TODO set flag for status in DB
+        console.log(deleteMessage);
+    });
 });
