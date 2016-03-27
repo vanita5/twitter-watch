072c9602421630334c8a09ec888783e39446bd6c
vanita5 <mail@vanita5.de>
2016-03-27 01:21:08 +0100

    init background script

---
+++ background/index.js
@@ -0,0 +1,10 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/twitter-watch';
+
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log('Connected correctly to server.');
+    db.close();
+});
