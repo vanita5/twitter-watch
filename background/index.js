fd4491a5b6677cca5857a18e85d39702622c463b
vanita5 <mail@vanita5.de>
2016-03-27 17:24:53 +0200

    accounts being stored in the database, improve background script, close #1

--- background/index.js
+++ background/index.js
@@ -1,24 +1,9 @@
 var MongoClient = require('mongodb').MongoClient,
-    accounts = require('require-all')(__dirname + '/../config/accounts'),
+    account_configs = require('require-all')(__dirname + '/../config/accounts'),
     twitter_config = require(__dirname + '/../config/twitter'),
     Twit = require('twit');
 
 const url = require(__dirname + '/../config/mongo').url;
-
-var arr_accounts = [];
-Object.keys(accounts).forEach(function(file_key) {
-    Object.keys(accounts[file_key]).forEach(function(account_key) {
-        if (account_key.charAt(0) === '_') return;
-        arr_accounts.push(accounts[file_key][account_key].id_str);
-    });
-});
-
-var T = new Twit({
-    consumer_key: twitter_config.auth.consumer_key,
-    consumer_secret: twitter_config.auth.consumer_secret,
-    access_token: twitter_config.auth.access_token,
-    access_token_secret: twitter_config.auth.access_token_secret
-});
 
 MongoClient.connect(url, function(err, db) {
     if (err) {
@@ -27,11 +12,61 @@ MongoClient.connect(url, function(err, db) {
     console.log('Connected correctly to server.');
 
     var tweets = db.collection('tweets');
+    var accounts = db.collection('accounts');
 
-    var stream = T.stream('statuses/filter', { follow: arr_accounts });
+    console.log('Empty old accounts collection...');
+    accounts.deleteMany({}, function(err, result) {
+        if (err) console.error(err);
+
+        var docs = [];
+        Object.keys(account_configs).forEach(function(file_key) {
+            Object.keys(account_configs[file_key]).forEach(function(account_key) {
+                if (account_key.charAt(0) === '_') return;
+                var account = account_configs[file_key][account_key];
+                docs.push(account);
+            });
+        });
+        console.log('Got %d accounts from JSON files.', docs.length);
+        accounts.insertMany(docs, {w:1}, function(err, result) {
+            if (err) console.error(err);
+
+            //validate
+            accounts.count(function(err, result) {
+                console.log('Inserted %d accounts!', result);
+
+                //collect account ids for filter stream
+                var account_ids = [];
+                accounts.find({}, { _id: 0, id_str: 1 }).toArray(function(err, result) {
+                    if (err) { console.error(err); return; }
+                    result.forEach(function(acc) {
+                        account_ids.push(acc.id_str);
+                    });
+                    initStream(account_ids, tweets);
+                });
+            });
+        });
+    });
+});
+
+var initStream = function(account_ids, tweets) {
+    console.log('Starting stream...');
+
+    var T = new Twit({
+        consumer_key: twitter_config.auth.consumer_key,
+        consumer_secret: twitter_config.auth.consumer_secret,
+        access_token: twitter_config.auth.access_token,
+        access_token_secret: twitter_config.auth.access_token_secret
+    });
+
+    var stream = T.stream('statuses/filter', { follow: account_ids });
 
     stream.on('tweet', function(tweet) {
         console.log('Inserting @' + tweet.user.screen_name + ': "' + tweet.text + '"');
+
+        //TODO Filter
+        //Filter retweets from unobserved accounts
+        //Filter mentions targeting observed accounts
+
         tweets.insertOne(tweet, {w:1}, function(err, result) {
             if (err) console.error(err);
         });
@@ -46,4 +81,4 @@ MongoClient.connect(url, function(err, db) {
             }
         );
     });
-});
+};
