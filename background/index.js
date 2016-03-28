60e001bebe33141b777f6569c382d230fcb131dc
remmy hume <rem@remics.net>
2016-03-28 05:49:11 +0200

    ES6 refactoring

--- background/index.js
+++ background/index.js
@@ -1,11 +1,19 @@
-var MongoClient = require('mongodb').MongoClient,
+'use strict';
+const MongoClient = require('mongodb').MongoClient,
     account_configs = require('require-all')(__dirname + '/../config/accounts'),
     twitter_config = require(__dirname + '/../config/twitter'),
-    Twit = require('twit');
+    Twit = require('twit'),
+    url = require(__dirname + '/../config/mongo').url;
 
-const url = require(__dirname + '/../config/mongo').url;
 
-MongoClient.connect(url, function(err, db) {
+const T = new Twit({
+    consumer_key: twitter_config.auth.consumer_key,
+    consumer_secret: twitter_config.auth.consumer_secret,
+    access_token: twitter_config.auth.access_token,
+    access_token_secret: twitter_config.auth.access_token_secret
+});
+
+MongoClient.connect(url, (err, db) => {
     if (err) {
         return console.dir(err);
     }
@@ -15,67 +23,52 @@ MongoClient.connect(url, function(err, db) {
     var accounts = db.collection('accounts');
 
     console.log('Empty old accounts collection...');
-    accounts.deleteMany({}, function(err, result) {
-        if (err) console.error(err);
-
-        var docs = [];
-        Object.keys(account_configs).forEach(function(file_key) {
-            Object.keys(account_configs[file_key]).forEach(function(account_key) {
-                if (account_key.charAt(0) === '_') return;
-                var account = account_configs[file_key][account_key];
-                docs.push(account);
-            });
-        });
-        console.log('Got %d accounts from JSON files.', docs.length);
-        accounts.insertMany(docs, {w:1}, function(err, result) {
-            if (err) console.error(err);
-
+    accounts.deleteMany({})
+        .catch((e) => {console.error(e);})
+        .then(() => {
+            //collect accounts from jsons and save them into the db
+            var docs = [];
+            for(var file_key in account_configs) {
+                for(var account_key in account_configs[file_key]) {
+                    if (account_key.charAt(0) === '_') continue;
+                    var account = account_configs[file_key][account_key];
+                    docs.push(account);
+                }
+            }
+            console.log('Got %d accounts from JSON files.', docs.length);
+            return accounts.insertMany(docs, {w:1});
+        })
+        .catch((e) => {console.error(e);})
+        .then(() => {
             //collect account ids for filter stream
-            var account_ids = [];
-            accounts.find({}, { _id: 0, id_str: 1 }).toArray(function(err, result) {
-                if (err) { console.error(err); return; }
-                result.forEach(function(acc) {
-                    account_ids.push(acc.id_str);
-                });
-                initStream(account_ids, tweets);
-            });
+            return accounts.find({}, { _id: 0, id_str: 1 }).toArray();
+        })
+        .catch((e) => {console.error(e);})
+        .then((account_ids) => {
+            initStream(account_ids, tweets);
         });
-    });
 });
 
-var initStream = function(account_ids, tweets) {
+var initStream = function (account_ids, tweets) {
     console.log('Starting stream...');
-
-    var T = new Twit({
-        consumer_key: twitter_config.auth.consumer_key,
-        consumer_secret: twitter_config.auth.consumer_secret,
-        access_token: twitter_config.auth.access_token,
-        access_token_secret: twitter_config.auth.access_token_secret
-    });
 
     var stream = T.stream('statuses/filter', { follow: account_ids });
 
-    stream.on('tweet', function(tweet) {
+    stream.on('tweet', (tweet) => {
         if (filter(tweet, account_ids)) return;
-
         console.log('Inserting @' + tweet.user.screen_name + ': "' + tweet.text + '"');
-        tweets.insertOne(tweet, {w:1}, function(err, result) {
-            if (err) console.error(err);
-        });
+        tweets.insertOne(tweet, {w:1})
+            .catch((e) => {console.error(e);});
     });
 
-    stream.on('delete', function(deleteMessage) {
-        tweets.updateOne(
-            { "id_str": deleteMessage.delete.status.id_str },
-            {$set: { "deleted": true }},
-            function(err, result) {
-                if (err) console.error(err);
-            }
-        );
+    stream.on('delete', (deleteMessage) => {
+        tweets.updateOne({"id_str": deleteMessage.delete.status.id_str},
+                         {$set: { "deleted": true }})
+            .catch((e) => {console.error(e);});
     });
 };
 
-var filter = function(tweet, accounts_ids) {
+var filter = function (tweet, accounts_ids) {
 
     //case 1: tweet.user.id_str is NOT an observed account -> this is a mention or a retweet from someone else
     if (accounts_ids.indexOf(tweet.user.id_str) < 0) return true;
